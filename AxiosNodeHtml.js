9a6d953a5d0176b3af93ba3bbc5cc5883ab021f5
Pranpriya <s6506021622027@email.kmutnb.ac.th>
2024-02-12 10:08:20 +0700

    css

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const app = express()
 var bodyParser = require('body-parser')
 
-const base_url = "http://10.104.17.148"
+const base_url = "http://localhost:3000"
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:false}))
