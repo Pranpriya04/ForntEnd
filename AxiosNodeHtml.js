80955a3d92fbd89f2a70257e0febad862d8a0a51
Pranpriya <s6506021622027@email.kmutnb.ac.th>
2024-02-12 09:39:58 +0700

    ttt

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const app = express()
 var bodyParser = require('body-parser')
 
-const base_url = "10.104.17.148"
+const base_url = "http://10.104.17.148"
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:false}))
